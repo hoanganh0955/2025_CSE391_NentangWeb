83466927edb5892876d3b9bc31a55c1ef1713adb
Hoàng Anh <85402475+hoanganh0955@users.noreply.github.com>
2025-06-07 02:27:51 +0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,20 +7,25 @@ function Home() {
     document.title = `Count: ${count}`;
   }, [count]);
 
-  const handleReset = () => {
-    setCount(0);
-  };
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
+  const reset = () => setCount(0);
 
   return (
     <div className="page center">
       <h1 className="title">Home Page</h1>
       <p>Count: {count}</p>
-      <button onClick={() => setCount((prev) => prev + 1)} className="btn">
-        Increment
-      </button>
-      <button onClick={handleReset} className="btn" style={{ marginLeft: "1px", marginTop: "10px" }}>
-        Reset
-      </button>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <button onClick={increment} className="btn">
+          Increment
+        </button>
+        <button onClick={decrement} className="btn">
+          Decrement
+        </button>
+        <button onClick={reset} className="btn">
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
